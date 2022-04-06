82c163e3d177ba7cc76a4e88ba1888acc16d3fcd
Randall Wilk <wilk.randall@gmail.com>
2022-04-06 14:50:38 -0500

    Make compatible with Laravel 7

--- src/Support/Str.php
+++ src/Support/Str.php
@@ -12,4 +12,16 @@ class Str extends BaseStr
     {
         return mb_substr($value, 0, 1);
     }
+
+    /**
+     * This method can be removed if we ever remove support for Laravel 7.*.
+     */
+    public static function squish($value)
+    {
+        if (method_exists(BaseStr::class, 'squish')) {
+            return parent::squish($value);
+        }
+
+        return preg_replace('/\s+/', ' ', trim($value));
+    }
 }
