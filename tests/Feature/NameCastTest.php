70b6f1fc278302dfef582b26b2d0cdfdd4fb4bf1
Randall Wilk <wilk.randall@gmail.com>
2023-05-16 15:10:13 -0500

    Don't attempt to store computed columns to the database

--- tests/Feature/NameCastTest.php
+++ tests/Feature/NameCastTest.php
@@ -12,7 +12,9 @@ it('casts to a name', function () {
         ->and($model->name->first)->toBe('John')
         ->and($model->name->last)->toBe('Smith')
         ->and($model->name->full)->toBe('John Smith')
-        ->and((string) $model->name)->toBe('John Smith');
+        ->and((string) $model->name)->toBe('John Smith')
+        ->and($model->getDirty())->toHaveKey('name')
+        ->and($model->getDirty()['name'])->toBe('John Smith');
 });
 
 it('can be casted from first and last name on a model', function () {
@@ -29,4 +31,14 @@ test('different first and last name attributes can be used', function () {
     expect($model->custom_name->full)->toBe('John Smith')
         ->and($model->custom_name->first)->toBe('John')
         ->and($model->custom_name->last)->toBe('Smith');
+});
+
+it('will not attempt to store a computed column to the database if it is accessed before saving the model', function () {
+    $model = new Model(['given_name' => 'John', 'family_name' => 'Smith']);
+
+    $model->custom_name;
+
+    expect($model->getDirty())->not->toHaveKey('custom_name')
+        ->toHaveKey('given_name')
+        ->toHaveKey('family_name');
 });
